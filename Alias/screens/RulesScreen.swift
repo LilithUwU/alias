14d27f8956d9b826cabc6c0577a7afe5e46a24b6
Lilith <lilith.takiryan@gmail.com>
2026-06-15 12:36:00 +0400

    update: comic style zoomable image for rules screen

--- Alias/screens/RulesScreen.swift
+++ Alias/screens/RulesScreen.swift
@@ -7,19 +7,35 @@ import SwiftUI
 struct RulesScreen: View {
     @State var rules: String = "Loading..."
     @StateObject private var languageUtil = LanguageUtil()
-    
+    @GestureState private var scale: CGFloat = 1.0
+        @State private var finalScale: CGFloat = 1.0
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
             GridBackground().blur(radius: 2)
             
-            ScrollView {
-                Text(rules.replacingOccurrences(of: "\\n", with: "\n"))
-                    .font(.body)
-                    .lineSpacing(4)
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
+            Image("rules")
+                       .resizable()
+                       .scaledToFit()
+                       .padding()
+                       .scaleEffect(scale * finalScale)
+                       .gesture(
+                           MagnifyGesture()
+                               .updating($scale) { value, gestureState, _ in
+                                   gestureState = value.magnification
+                               }
+                               .onEnded { value in
+                                   finalScale *= value.magnification
+                               }
+                       )
+            
+//            ScrollView {
+//                Text(rules.replacingOccurrences(of: "\\n", with: "\n"))
+//                    .font(.body)
+//                    .lineSpacing(4)
+//                    .padding()
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//            }
         }
         .navigationTitle("Rules")
         .onAppear {
